00bb4ad329b912d556b80d30f471f8fdf4c57b8d
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-07-07 11:16:19 +0100

    move the books state to index.js file

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 
 class BooksForm extends React.Component {
@@ -12,7 +13,6 @@ class BooksForm extends React.Component {
 
   render() {
     const categories = ['', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-    const { title, category } = this.state;
     return (
       <form className="w-full max-w-sm">
         <div className="md:flex md:items-center mb-6">
@@ -23,7 +23,7 @@ class BooksForm extends React.Component {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="booktitle" id="booktitle" value={title} />
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="booktitle" id="booktitle" />
           </div>
           <div className="relative">
             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="cat">
@@ -31,7 +31,7 @@ class BooksForm extends React.Component {
               Category:
             </label>
 
-            <select className="flex ml-5 appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="cat" value={category}>
+            <select className="flex ml-5 appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="cat">
               {categories.map(cat => (
                 <option key={cat} value={cat}>
                   {' '}
